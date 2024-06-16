73267cb2358cf9ef20310b16b79d6c2a5c2eb448
rafi1363 <ryuta130603@gmail.com>
2024-06-16 17:45:54 +0700

    tugas modul 6 machine learning

---
+++ chat-apps-212310019/src/apps/BaseRoute.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { ChapterOne } from "../modules/chapter-1";
+import { ChapterTwo } from "../modules/chapter-2";
+import SignIn from "../modules/chapter-2/widgets/auth";
+import LayoutInit from "../components/LayoutInit";
+import { ChapterThree } from "../modules/chapter-3";
+
+export default function BaseRoute() {
+  const panco = [
+    { path: "chapter-1", element: <ChapterOne /> },
+    { path: "chapter-2", element: <ChapterTwo /> },
+    { path: "chapter-3", element: <ChapterThree /> },
+  ];
+  return (
+    <React.Suspense>
+      <Routes>
+        <Route
+          index
+          element={
+            <LayoutInit>
+              <ChapterOne />
+            </LayoutInit>
+          }
+        />
+        <Route path="chapter-two" element={<ChapterTwo />} />
+        <Route path="chap3" element={<ChapterThree />} />
+        <Route path="sign-in" element={<SignIn />} />
+        {panco.map((a) => {
+          <Route path={a.path} element={a.element} />;
+        })}
+      </Routes>
+    </React.Suspense>
+  );
+}
